9508271ad41a2c059875d48c2a55f416ed3def8e
DanHack <danilo.peres5632@gmail.com>
2018-02-12 12:00:35 -0200

    Create tests for method document replace

--- spec/replace_it_spec.rb
+++ spec/replace_it_spec.rb
@@ -1,11 +1,29 @@
 require "spec_helper"
+require 'pry'
 
 describe ReplaceIt do
-  it "has a version number" do
-    expect(ReplaceIt::VERSION).not_to be nil
+  let(:file_name)   { 'testfile.txt' }
+  let(:temp_file)   { 'testtemfile.txt' }
+  let(:content)     { 'aaa' }
+  let(:new_content) { 'bbb' }
+  let(:replace)     { ReplaceDocument.new(file_name, temp_file) }
+
+  around(:each) do |example|
+    File.open(file_name, 'w') { |f| f.write(content) }
+    example.run
+    File.delete(file_name)
+    File.delete(temp_file)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'document_replace method should keep the original file' do
+    replace.document_replace(content, new_content)
+
+    expect(File.open(file_name).read.strip).to eq content
+  end
+
+  it 'document_replace method should replace a string' do
+    replace.document_replace(content, new_content)
+
+    expect(File.open(temp_file).read.strip).to eq new_content
   end
 end
