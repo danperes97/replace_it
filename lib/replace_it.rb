36552cdb77070be4c23fe4613c8305eff5e4a95c
DanHack <danilo.peres5632@gmail.com>
2018-02-12 11:06:57 -0200

    ReplaceIt module method replace document working

--- lib/replace_it.rb
+++ lib/replace_it.rb
@@ -1,15 +1,9 @@
 require "replace_it/version"
 
 module ReplaceIt
-  def document_replace!(file_name:, string:, new_string:)
-    File.open(file_name, 'w') do |file|
-      file.puts File.read(file_name).gsub(/#{string}/, new_string)
-    end
-  end
-
-  def document_replace(original_file:, temp_file:, options = {})
-    File.open(temp_file, 'w') do |file|
-      file.puts File.read(original_file).gsub(/#{string}/, new_string)
+  def self.document_replace(options = {})
+    File.open(options[:temp_file], 'w') do |file|
+      file.puts File.read(options[:original_file]).gsub(/#{options[:string]}/, options[:new_string])
     end
   end
 end
