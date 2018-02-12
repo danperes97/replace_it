2864f3a37a5e3366eb7841ea4890c85e5cfd83c9
DanHack <danilo.peres5632@gmail.com>
2018-02-12 11:21:39 -0200

    Fix replace document

--- lib/replace_it.rb
+++ lib/replace_it.rb
@@ -1,9 +1,16 @@
 require "replace_it/version"
 
-module ReplaceIt
-  def self.document_replace(options = {})
-    File.open(options[:temp_file], 'w') do |file|
-      file.puts File.read(options[:original_file]).gsub(/#{options[:string]}/, options[:new_string])
+class ReplaceDocument
+  attr_accessor :original_file, :temp_file
+
+  def initialize(original_file, temp_file)
+    @original_file = original_file
+    @temp_file     = temp_file
+  end
+
+  def document_replace(string, new_string)
+    File.open(@temp_file, 'w') do |file|
+      file.puts File.read(@original_file).gsub(/#{string}/, new_string)
     end
   end
 end
